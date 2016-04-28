e7a7661b007b94ab2b5a6ce7ae14edca11f9bda2
rahulrainahome <Password123>
2016-04-28 11:35:07 +0530

    Initial commit.

--- app/src/main/java/visitor/app/visitorapp/FormActivity.java
+++ app/src/main/java/visitor/app/visitorapp/FormActivity.java
@@ -2,8 +2,11 @@ package visitor.app.visitorapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class FormActivity extends AppCompatActivity {
+
+    EditText txtName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
