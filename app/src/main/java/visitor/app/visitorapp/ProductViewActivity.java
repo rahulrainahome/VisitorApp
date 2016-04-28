3739805cc81f8f5902fe493f8546c47f3445a4e5
rahulrainahome <Password123>
2016-04-28 12:30:38 +0530

    Initial Commit

--- app/src/main/java/visitor/app/visitorapp/ProductViewActivity.java
+++ app/src/main/java/visitor/app/visitorapp/ProductViewActivity.java
@@ -1,13 +1,25 @@
 package visitor.app.visitorapp;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.InputType;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
 
-public class ProductViewActivity extends AppCompatActivity {
+import java.util.ArrayList;
+
+public class ProductViewActivity extends AppCompatActivity implements AdapterView.OnItemLongClickListener {
+
+    ListView lw;
+    ArrayAdapter<String> adapter = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,14 +28,69 @@ public class ProductViewActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        adapter = new ArrayAdapter<String>(ProductViewActivity.this, android.R.layout.simple_list_item_1, Constants.list);
+        lw.setAdapter(adapter);
+        lw.setOnItemLongClickListener(this);
+
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+
             }
         });
     }
 
+    @Override
+    public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+
+        Snackbar.make(view, "Are you sure to delete this ?", Snackbar.LENGTH_LONG)
+                .setAction("Action", new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        //delete the item from activity.
+                    }
+                }).show();
+        return true;
+    }
+
+    /**
+     * @method: createCategory
+     * @desc: Creates input AlertDialog to inout category string and then inserts in the SQLite DB.
+     */
+    private void createCategory()
+    {
+        //Create AlertDialog
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("New Category Name");
+        builder.setCancelable(false);
+
+        // Set up the EditText.
+        final EditText input = new EditText(this);
+
+        input.setInputType(InputType.TYPE_CLASS_TEXT);
+        builder.setView(input);
+
+        // Set up the buttons
+        builder.setPositiveButton("Add", new DialogInterface.OnClickListener()
+            {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+
+                }
+            }
+        );
+
+        builder.setNegativeButton("Discard", new DialogInterface.OnClickListener()
+
+            {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+
+                    dialog.cancel();
+                }
+            }
+        );
+        builder.show();
+    }
 }
