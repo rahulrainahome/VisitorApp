345382844188cb63544f75e70c518174eaaf59a5
rahulrainahome <Password123>
2016-04-28 12:32:08 +0530

    Initial Commit

--- app/src/main/java/visitor/app/visitorapp/ProductViewActivity.java
+++ app/src/main/java/visitor/app/visitorapp/ProductViewActivity.java
@@ -36,7 +36,7 @@ public class ProductViewActivity extends AppCompatActivity implements AdapterVie
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                createCategory();
             }
         });
     }
