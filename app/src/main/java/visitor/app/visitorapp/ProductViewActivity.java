4cb5fee7a33fb3c8622024417b343555b1bb146a
rahulrainahome <rahulrainahome@live.com>
2016-05-01 01:59:18 +0530

    Initial Commit

--- app/src/main/java/visitor/app/visitorapp/ProductViewActivity.java
+++ app/src/main/java/visitor/app/visitorapp/ProductViewActivity.java
@@ -26,6 +26,7 @@ public class ProductViewActivity extends AppCompatActivity implements AdapterVie
     ListView lw;
     ArrayAdapter<String> adapter = null;
     ArrayList<String> list = null;
+    SharedPreferences s = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,7 +37,7 @@ public class ProductViewActivity extends AppCompatActivity implements AdapterVie
         lw = (ListView)findViewById(R.id.listView1);
 
         //Get Product Interest data from Shared Prederences.
-        SharedPreferences s = getSharedPreferences(Constants.pref_prod, 0);
+        s = getSharedPreferences(Constants.pref_prod, 0);
         String prodInt ="" + s.getString("data","[]");
         list = new ArrayList<String>();
 
@@ -68,14 +69,44 @@ public class ProductViewActivity extends AppCompatActivity implements AdapterVie
     }
 
     @Override
-    public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+    public boolean onItemLongClick(AdapterView<?> parent, View view, final int position, long id) {
 
-        Snackbar.make(view, "Are you sure to delete this ?", Snackbar.LENGTH_LONG)
-                .setAction("Action", new View.OnClickListener() {
+        Snackbar.make(view, "Are you sure to delete this?", Snackbar.LENGTH_LONG)
+                .setAction("Yes ?", new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
-                        //delete the item from activity.
+
+                        try {
+                            //Get the old data from cache and remove selected data and again save in cache.
+                            String oldStr = s.getString("data", "[]");
+                            JSONArray json = new JSONArray(oldStr);
+                            JSONArray newjson = new JSONArray();
+                            for(int a = 0; a < json.length(); a++)
+                            {
+                                if(a == position)
+                                {
+                                    continue;
+                                }
+                                newjson.put("" + json.getString(a));
+                            }
+                            SharedPreferences.Editor se = getSharedPreferences(Constants.pref_prod, 0).edit();
+                            se.putString("data", newjson.toString());
+                            se.commit();
+
+                            //Again get data from cache and parse into list and reload listView.
+                            json = new JSONArray(s.getString("data", "[]"));
+                            list.clear();
+                            for(int a = 0; a < json.length(); a++)
+                            {
+                                list.add("" + json.getString(a));
+                            }
+                            adapter.notifyDataSetChanged();
+                            Toast.makeText(getApplicationContext(), "Successfully Deleted.", Toast.LENGTH_SHORT).show();
+                        } catch (Exception e) {
+                            Toast.makeText(getApplicationContext(), "Exception: Some internal error ocurred.", Toast.LENGTH_LONG).show();
+                        }
                     }
+
                 }).show();
         return true;
     }
@@ -88,7 +119,7 @@ public class ProductViewActivity extends AppCompatActivity implements AdapterVie
     {
         //Create AlertDialog
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
-        builder.setTitle("New Category Name");
+        builder.setTitle("New Product Interest");
         builder.setCancelable(false);
 
         // Set up the EditText.
@@ -98,24 +129,41 @@ public class ProductViewActivity extends AppCompatActivity implements AdapterVie
         builder.setView(input);
 
         // Set up the buttons
-        builder.setPositiveButton("Add", new DialogInterface.OnClickListener()
-            {
-                @Override
-                public void onClick(DialogInterface dialog, int which) {
+        builder.setPositiveButton("Add", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
 
+                        String newStr = input.getText().toString();
+                        try {
+                            String oldStr = s.getString("data", "[]");
+                            JSONArray json = new JSONArray(oldStr);
+                            json.put("" + newStr);
+                            SharedPreferences.Editor se = getSharedPreferences(Constants.pref_prod, 0).edit();
+                            se.putString("data", json.toString());
+                            se.commit();
+                            json = new JSONArray(s.getString("data", "[]"));
+                            list.clear();
+                            for (int a = 0; a < json.length(); a++) {
+                                list.add("" + json.getString(a));
+                            }
+                            adapter.notifyDataSetChanged();
+                            Toast.makeText(getApplicationContext(), "Successfully Added.", Toast.LENGTH_SHORT).show();
+                        } catch (Exception e) {
+                            Toast.makeText(getApplicationContext(), "Exception: Some internal error ocurred.", Toast.LENGTH_LONG).show();
+                        }
+                    }
                 }
-            }
         );
 
         builder.setNegativeButton("Discard", new DialogInterface.OnClickListener()
 
-            {
-                @Override
-                public void onClick(DialogInterface dialog, int which) {
+                {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
 
-                    dialog.cancel();
+                        dialog.cancel();
+                    }
                 }
-            }
         );
         builder.show();
     }
